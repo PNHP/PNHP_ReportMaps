aa76686dce584b5b619e7ccdfd016cafed352736
Christopher Tracey <ctracey@paconserve.org>
2017-05-25 20:20:25 -0400

    fixed title italics

--- code.R
+++ code.R
@@ -18,35 +18,34 @@ eo.nonsensitive@data$extant[eo.nonsensitive@data$lastyear<1992] <- "Historic"
 eo.nonsensitive@data$extant[eo.nonsensitive@data$lastyear>=1992] <- "Extant"
 eo@data$extant[is.na(eo@data$lastyear)] <- "Historic"
 # the following lines just add a color based on the above categoires// might be good to change this do at the same time as the above three
-eo.nonsensitive@data$color[eo.nonsensitive@data$lastyear<1992] <- "#006837"
-eo.nonsensitive@data$color[eo.nonsensitive@data$lastyear>=1992] <- "#78C679"
-eo.nonsensitive@data$color[is.na(eo.nonsensitive@data$lastyear)] <- "#006837"
+eo.nonsensitive@data$color[eo.nonsensitive@data$lastyear<1992] <- "#f44d41"
+eo.nonsensitive@data$color[eo.nonsensitive@data$lastyear>=1992] <- "#4286f4"
+eo.nonsensitive@data$color[is.na(eo.nonsensitive@data$lastyear)] <- "#f44d41"
+# the following lines just add a plot symbol based on the above categoires// might be good to change this do at the same time as the above three
+eo.nonsensitive@data$pchsym[eo.nonsensitive@data$lastyear<1992] <- 19 #circle
+eo.nonsensitive@data$pchsym[eo.nonsensitive@data$lastyear>=1992] <- 17 # triangle
+eo.nonsensitive@data$pchsym[is.na(eo.nonsensitive@data$lastyear)] <- 19 # circle
 
 #get a list of snames to run the loop
-snames.nonsensitive <- unique(eo.nonsensitive@data$SNAME)
+snames.nonsensitive <- eo.nonsensitive@data[c("SNAME","SCOMNAME")]
+snames.nonsensitive <- unique(snames.nonsensitive)
 
-for (i in 1:length(snames.nonsensitive)) {
-  tmp <- eo.nonsensitive[eo.nonsensitive$SNAME == as.character(snames.nonsensitive[i]), ] 
-  ##png(file=paste("maps/",snames.nonsensitive[i],".png",sep="")) # needs to go before the plot
-  plot(county)
-  plot(tmp, add=T, pch = 16, cex=0.75, col=eo.nonsensitive@data$color)
-  #eo.nonsensitive@data$SNAME <- droplevels(eo.nonsensitive@data$SNAME)
-  title(snames.nonsensitive[i], cex=0.5)
-  
-  type <- eo.nonsensitive@data$extant
-  col <- rep(x="#006837")
-  col[type="Historic"] <- "#006837"
-  
-  #legend(x="bottom",legend=unique(eo.nonsensitive@data$extant), col=unique(as.numberic(eo.nonsensitive@data$extant)), cex = 1, bty="n")  #c("#78C679","#006837")
+for (i in 1:length(snames.nonsensitive$SNAME)) {
+  tmp <- eo.nonsensitive[eo.nonsensitive$SNAME == as.character(snames.nonsensitive$SNAME[i]), ] 
+  png(file=paste("maps/",snames.nonsensitive$SNAME[i],".png",sep=""), width=500, height=300) # needs to go before the plot  #
+  par(mar=c(2,2,2,2)+0.1)
+  names1 <- c(as.character(snames.nonsensitive$SCOMNAME[i]),as.character(snames.nonsensitive$SNAME[i]))
+  plot(county, main=bquote( .(names1[1]) ~ "(" * italic(.(names1[2])) * ")"  ))
+  plot(tmp, add=T, pch =eo.nonsensitive@data$pchsym, cex=1, col=eo.nonsensitive@data$color)
+  legend(x="bottom",legend=c("Historic","Extant"), col=c("#f44d41","#4286f4"), pch=c(19,17), cex=1, bty="n", ncol=2)  #c("#78C679","#006837")
   #writeOGR(tmp, dsn=getwd(), snames[i], driver="ESRI Shapefile", overwrite_layer=TRUE)
-  ##dev.off() # turns off the plotter writing to pngs
-
+  dev.off() # turns off the plotter writing to pngs
 }
 
 
 ############################
 ### Still Under Development ###
-# this code deals with sensitve speciesto make maps that filled in by the county instead of dots
+# this code deals with sensitve species to make maps that filled in by the county instead of dots
 # subset to sensitive species
 eo.sensitive <- eo[eo$SENSITV_SP=="Y",]
 
