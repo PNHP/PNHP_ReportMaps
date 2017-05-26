2d454edcd888ab8e4907359383053ab725234849
Christopher Tracey <ctracey@paconserve.org>
2017-05-26 11:24:39 -0400

    date change, fix sizing, etc

--- code.R
+++ code.R
@@ -1,30 +1,35 @@
 setwd("C:/Users/ctracey/Dropbox (PNHP @ WPC)/FactsheetMaps")
 
 library(rgdal)
-#library(spatialEco)
-
+library(spatialEco)
 
 # load shapefiles. both should be in the same projection
 county <- readOGR("C:/Users/ctracey/Dropbox (PNHP @ WPC)/FactsheetMaps","PaCounty")
 eo <-  readOGR("C:/Users/ctracey/Dropbox (PNHP @ WPC)/FactsheetMaps","EO_ptreps") # shuold have standard biotics fields. 
 
-# subset to nonsensitive species for dot based maps
-eo.nonsensitive <- eo[eo$SENSITV_SP=="N",]
+# get the current year
+currentyear <- as.numeric(format(Sys.Date(), format="%Y"))
+extantyear <- currentyear - 25
 
 #gets the year, the as.numeric seems to turn all 'no date' and 'pre-' values to NA
-eo.nonsensitive@data$lastyear <- as.numeric(substr(eo.nonsensitive@data$LASTOBS,1,4))
+eo@data$lastyear <- as.numeric(substr(eo@data$LASTOBS,1,4))
 # assign legends and colors for the maps
-eo.nonsensitive@data$extant[eo.nonsensitive@data$lastyear<1992] <- "Historic"
-eo.nonsensitive@data$extant[eo.nonsensitive@data$lastyear>=1992] <- "Extant"
+eo@data$extant[eo@data$lastyear<extantyear] <- "Historic"
+eo@data$extant[eo@data$lastyear>=extantyear] <- "Extant"
 eo@data$extant[is.na(eo@data$lastyear)] <- "Historic"
 # the following lines just add a color based on the above categoires// might be good to change this do at the same time as the above three
-eo.nonsensitive@data$color[eo.nonsensitive@data$lastyear<1992] <- "#f44d41"
-eo.nonsensitive@data$color[eo.nonsensitive@data$lastyear>=1992] <- "#4286f4"
-eo.nonsensitive@data$color[is.na(eo.nonsensitive@data$lastyear)] <- "#f44d41"
+eo@data$color[eo@data$lastyear<extantyear] <- "#f44d41"
+eo@data$color[eo@data$lastyear>=extantyear] <- "#4286f4"
+eo@data$color[is.na(eo@data$lastyear)] <- "#f44d41"
 # the following lines just add a plot symbol based on the above categoires// might be good to change this do at the same time as the above three
-eo.nonsensitive@data$pchsym[eo.nonsensitive@data$lastyear<1992] <- 19 #circle
-eo.nonsensitive@data$pchsym[eo.nonsensitive@data$lastyear>=1992] <- 17 # triangle
-eo.nonsensitive@data$pchsym[is.na(eo.nonsensitive@data$lastyear)] <- 19 # circle
+eo@data$pchsym[eo@data$lastyear<extantyear] <- 19 #circle
+eo@data$pchsym[eo@data$lastyear>=extantyear] <- 17 # triangle
+eo@data$pchsym[is.na(eo@data$lastyear)] <- 19 # circle
+
+# subset to nonsensitive species for dot based maps
+eo.nonsensitive <- eo[eo$SENSITV_SP=="N",]
+writeOGR(eo.nonsensitive, dsn=getwd(), "eo.nonsensitive", driver="ESRI Shapefile", overwrite_layer=TRUE)
+eo.sensitive <- readOGR(dsn=getwd(), "eo.nonsensitive")
 
 #get a list of snames to run the loop
 snames.nonsensitive <- eo.nonsensitive@data[c("SNAME","SCOMNAME")]
@@ -37,7 +42,7 @@ for (i in 1:length(snames.nonsensitive$SNAME)) {
   names1 <- c(as.character(snames.nonsensitive$SCOMNAME[i]),as.character(snames.nonsensitive$SNAME[i]))
   plot(county, main=bquote( .(names1[1]) ~ "(" * italic(.(names1[2])) * ")"  ))
   plot(tmp, add=T, pch =eo.nonsensitive@data$pchsym, cex=1, col=eo.nonsensitive@data$color)
-  legend(x="bottom",inset=-0.03,legend=c("Historic","Extant"), col=c("#f44d41","#4286f4"), pch=c(19,17), cex=1, bty="n", ncol=2)  #c("#78C679","#006837")
+  legend(x="bottom",inset=-0.025,legend=c(paste("Records pre-",extantyear,sep=""),paste("Records post-",extantyear,sep="")), col=c("#f44d41","#4286f4"), pch=c(19,17), cex=1, bty="n", ncol=2)  #
   #writeOGR(tmp, dsn=getwd(), snames[i], driver="ESRI Shapefile", overwrite_layer=TRUE)
   dev.off() # turns off the plotter writing to pngs
 }
@@ -48,22 +53,40 @@ for (i in 1:length(snames.nonsensitive$SNAME)) {
 # this code deals with sensitve species to make maps that filled in by the county instead of dots
 # subset to sensitive species
 eo.sensitive <- eo[eo$SENSITV_SP=="Y",]
+writeOGR(eo.sensitive, dsn=getwd(), "eo.sensitive", driver="ESRI Shapefile", overwrite_layer=TRUE)
+eo.sensitive <- readOGR(dsn=getwd(), "eo.sensitive")
 
-#gets the year, the as.numeric seems to turn all 'no date' and 'pre-' values to NA
-eo.sensitive@data$lastyear <- as.numeric(substr(eo.sensitive@data$LASTOBS,1,4))
-# assign legends and colors for the maps
-eo.sensitive@data$extant[eo.sensitive@data$lastyear<1992] <- "Historic"
-eo.sensitive@data$extant[eo.sensitive@data$lastyear>=1992] <- "Extant"
-eo.sensitive@data$extant[is.na(eo.sensitive@data$lastyear)] <- "Historic"
+eo.sensitive@data$EO_DATA <- NULL
+eo.sensitive@data$GEN_DESC <- NULL
+eo.sensitive@data$GENERL_COM <- NULL
+eo.sensitive@data$EORANKCOM <- NULL
+eo.sensitive@data$MGMT_COM <- NULL
+
+library(maptools)
+#get only the extant features
+eo.sensitive.extant <- eo.sensitive[eo.sensitive$extant=="Extant",]
+writeOGR(eo.sensitive.extant, dsn=getwd(), "eo.sensitive.extant", driver="ESRI Shapefile", overwrite_layer=TRUE)
+eo.sensitive.extant <- readOGR(dsn=getwd(), "eo.sensitive.extant")
+
+#get a list of snames to run the loop
+snames.sensitive.extant <- eo.sensitive.extant@data[c("SNAME","SCOMNAME")]
+snames.sensitive.extant <- unique(snames.sensitive.extant)
 
 
 
-#county_eo <- over(county,eo.sensitive)
+county_eo.extant <- over(eo.sensitive.extant,county,returnList = TRUE)
+
+
+
+county_eo <- over(eo.sensitive,county,returnList = TRUE)
+
+
+num.county_eo <- sapply(county_eo,length)
+county_eo2 <- spCbind(eo.sensitive,num.county_eo)
 
 library(spatialEco)
-
 # intersect points in polygon
-pts <- point.in.poly(pts, ply)
+county_eo <- point.in.poly(eo.sensitive,county)
 
 # check plot
 plot(ply)
@@ -74,20 +97,14 @@ pts<- as.data.frame(pts)
 
 
 
-## save for later
 
-# the following lines just add a color based on the above categoires// might be good to change this do at the same time as the above three
-eo.sensitive@data$color[eo.sensitive@data$lastyear<1992] <- "#f44d41"
-eo.sensitive@data$color[eo.sensitive@data$lastyear>=1992] <- "#4286f4"
-eo.sensitive@data$color[is.na(eo.sensitive@data$lastyear)] <- "#f44d41"
-# the following lines just add a plot symbol based on the above categoires// might be good to change this do at the same time as the above three
-eo.sensitive@data$pchsym[eo.sensitive@data$lastyear<1992] <- 19 #circle
-eo.sensitive@data$pchsym[eo.sensitive@data$lastyear>=1992] <- 17 # triangle
-eo.sensitive@data$pchsym[is.na(eo.sensitive@data$lastyear)] <- 19 # circle
+
+
+################
 
 #get a list of snames to run the loop
-snames.nonsensitive <- eo.sensitive@data[c("SNAME","SCOMNAME")]
-snames.nonsensitive <- unique(snames.nonsensitive)
+snames.sensitive <- eo.sensitive@data[c("SNAME","SCOMNAME")]
+snames.sensitive <- unique(snames.sensitive)
 
 
 
